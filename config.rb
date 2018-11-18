15ec146637bdcbdc9105dd6507cfc5d53ffd8f18
Pierre Renaudin <renaudin.pierre@gmail.com>
2018-11-18 21:34:23 +0100

    ajout google analytics

--- config.rb
+++ config.rb
@@ -23,10 +23,10 @@ end
 
 
 
-# activate :google_analytics do |ga|
-#     ga.tracking_id = UA-129392692-1
-#     ga.anonymize_ip = true
-#     ga.debug = false
-#     ga.development = false
-#     ga.minify = true
-# end
+activate :google_analytics do |ga|
+    ga.tracking_id = UA-129392692-1
+    ga.anonymize_ip = true
+    ga.debug = false
+    ga.development = false
+    ga.minify = true
+end

57d1d5cd56d6fce17768c324a452950f4fbe164e
RenaudinP <renaudin.pierre@gmail.com>
2018-12-20 17:03:02 +0100

    debug du javascript de la navbar

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,16 +1,16 @@
-function initUpdateNavbarOnScroll() {
-  const navbar = document.querySelector('.index .navbar-default');
+// function initUpdateNavbarOnScroll() {
+//   const navbar = document.querySelector('.index .navbar-default');
 
-  if (navbar) {
-    window.addEventListener('scroll', function() {
-      if (window.scrollY >= 120) {
-        navbar.classList.add('navbar-white');
-      } else {
-        navbar.classList.remove('navbar-white');
-      }
-    });
-  }
-}
+//   if (navbar) {
+//     window.addEventListener('scroll', function() {
+//       if (window.scrollY >= 120) {
+//         navbar.classList.add('navbar-white');
+//       } else {
+//         navbar.classList.remove('navbar-white');
+//       }
+//     });
+//   }
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   initUpdateNavbarOnScroll();
@@ -18,13 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-window.addEventListener('resize', function() {
-  if (window.matchMedia("(max-width: 700px)").matches) {
-    navbar.classList.add('navbar-white');
-  } else {
-    navbar.classList.remove('navbar-white');
-  }
-});
+
+
+
+// window.addEventListener('resize', function() {
+//   if (window.matchMedia("(max-width: 700px)").matches) {
+//     navbar.classList.add('navbar-white');
+//   } else {
+//     navbar.classList.remove('navbar-white');
+//   }
+// });
 
 
 // function initUpdateNavbarOnScroll() {
@@ -54,6 +57,26 @@ window.addEventListener('resize', function() {
 
 
 
+function initUpdateNavbarOnScroll() {
+const navbar = document.querySelector('.index .navbar-default');
+
+  if (navbar) {
+    if (window.matchMedia("(min-width: 800px)").matches) {
+          window.addEventListener('scroll', function() {
+            if (window.scrollY >= 120) {
+              navbar.classList.add('navbar-white');
+            } else {
+              navbar.classList.remove('navbar-white');
+            }
+          });
+    } else {
+      console.log("je suis dans le else");
+      const navbar = document.querySelector('.index .navbar-default');
+      console.log("je suis dans eaprès la constante");
+      navbar.classList.add('navbar-white');
+    }
+  }
+}
 
 
 

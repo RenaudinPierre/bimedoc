62dcc6fdb44b58ae1e928625ab5164766ae3630b
RenaudinP <renaudin.pierre@gmail.com>
2019-01-05 21:56:16 +0100

    rajout des cookies

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -18,45 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-
-
-// window.addEventListener('resize', function() {
-//   if (window.matchMedia("(max-width: 700px)").matches) {
-//     navbar.classList.add('navbar-white');
-//   } else {
-//     navbar.classList.remove('navbar-white');
-//   }
-// });
-
-
-// function initUpdateNavbarOnScroll() {
-//   const navbar = document.querySelector('.index .navbar-default');
-
-//   window.addEventListener('resize', function() {
-//     if (window.matchMedia("(min-width: 700px)").matches) {
-
-//       if (navbar) {
-//         window.addEventListener('scroll', function() {
-//           if (window.scrollY >= 120) {
-//             navbar.classList.add('navbar-white');
-//           } else {
-//             navbar.classList.remove('navbar-white');
-//           }
-//         });
-//       }
-//     } else {
-//       navbar.classList.add('navbar-white');
-//     }
-//   });
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   initUpdateNavbarOnScroll();
-// });
-
-
-
 function initUpdateNavbarOnScroll() {
 const navbar = document.querySelector('.index .navbar-default');
 
